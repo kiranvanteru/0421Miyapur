a5067490a7406ddded0130361f7aaed9929e148b
kiranvanteru <kk23vanteru@gmail.com>
2018-04-21 20:32:56 +0530

    miyapur0421

---
+++ Miyapur/src/test/java/maven0421/DemoTest.java
@@ -0,0 +1,24 @@
+package maven0421;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterTest;
+
+public class DemoTest {
+  @Test
+  public void f() {
+	  
+	  System.out.println("test execution");
+  }
+  @BeforeTest
+  public void beforeTest() {
+	  
+	  System.out.println("before test");
+  }
+
+  @AfterTest
+  public void afterTest() {
+	  System.out.println("after test");
+  }
+
+}
